86592ad17078f554d266c9e122f7483ccc6e09c1
Lakki <thakurluckysinghbrh@gmail.com>
2020-12-22 05:00:15 +0530

    js initialisation

---
+++ main.js
@@ -0,0 +1,32 @@
+const overlay=document.querySelector('.overlay')
+nt=document.getElementById('notice')
+let timeout
+function notice(message,time=false) {
+    overlay.style.display='flex';
+    nt.innerHTML=message;
+    if (time) {
+        if (typeof(timeout)) {
+            clearTimeout(timeout)
+        }
+        setTimeout(
+            ()=>{
+                fade()
+            },time
+        )
+    }
+};
+function dismiss(){
+overlay.style.display='none';
+}
+function fade(selector='.overlay') {
+    console.time('fade')
+    element=document.querySelector(selector);
+    if(element.style.opacity&&element.style.opacity<.1){
+        element.style.display='none';
+        element.style.opacity=1;
+        console.timeEnd('fade')    
+        return
+    }
+    element.style.opacity=(element.style.opacity||1)/1.08;
+    setTimeout(()=> fade(selector),20)
+}
